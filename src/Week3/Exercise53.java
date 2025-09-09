ca06b2ee2c59ffe62c5a9c0a5db65c6e4932f80c
Henrik <Henriksaega@voco.ee>
2025-09-09 22:55:50 +0300

    Exercise 53

---
+++ src/Week3/Exercise53.java
@@ -0,0 +1,15 @@
+package Week3;
+import java.util.Scanner;
+public class Exercise53 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String sõna = reader.nextLine();
+
+        System.out.print("Length of the word part: ");
+        int pikkus = Integer.parseInt(reader.nextLine());
+
+        System.out.print("Result: " + sõna.substring(0, pikkus));
+    }
+}
