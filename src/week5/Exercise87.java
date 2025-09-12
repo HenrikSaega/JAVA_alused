136f934554c76e2fc475ead7d1a8719acceeb7c8
Henrik <Henriksaega@voco.ee>
2025-09-12 10:04:19 +0300

    Exercise87.1

---
+++ src/week5/Exercise87.java
@@ -0,0 +1,12 @@
+package week5;
+
+public class Exercise87 {
+    public static void main(String[] args) {
+        Apartment studioManhattan = new Apartment(1, 16, 5500);
+        Apartment twoRoomsBrooklyn = new Apartment(2, 38, 4200);
+        Apartment fourAndKitchenBronx = new Apartment(3, 78, 2500);
+
+        System.out.println( studioManhattan.larger(twoRoomsBrooklyn) );
+        System.out.println( fourAndKitchenBronx.larger(twoRoomsBrooklyn) );
+    }
+}
