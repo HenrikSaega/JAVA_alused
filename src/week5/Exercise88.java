b3c28a25dde4ab1c9e505c992f11bcf9b19deed2
Henrik <Henriksaega@voco.ee>
2025-09-12 11:20:17 +0300

    Exercise88.3

--- src/week5/Exercise88.java
+++ src/week5/Exercise88.java
@@ -1,11 +1,28 @@
 package week5;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Exercise88
 {
     public static void main(String[] args) {
-        Student pekka = new Student("Pekka Mikkola", "013141590");
-        System.out.println("name: " + pekka.getName());
-        System.out.println("studentnumber: " + pekka.getStudentNumber());
-        System.out.println(pekka);
+        ArrayList<Student> students = new ArrayList<Student>();
+        ArrayList<Student> searchedStudents = new ArrayList<Student>();
+        Scanner input = new Scanner(System.in);
+        Student Carl = new Student("Carl Gustaf Mannerheim", "015696234");
+        Student Steve = new Student("Steve Jobs", "013672548");
+        Student Edsger = new Student("Edsger Dijkstra", "014662803");
+        students.add(Carl);
+        students.add(Steve);
+        students.add(Edsger);
+
+        System.out.print("Give search term: ");
+        String search = input.nextLine();
+        for(Student nimi :  students)
+            {
+                if(nimi.getName().contains(search))
+                 {
+                     searchedStudents.add(nimi);
+                     System.out.println(nimi);
+                }
+            }
     }
 }
