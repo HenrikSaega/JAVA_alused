3421f3ccf77e52e2d0d6c63fdf8c6f00286ab184
Henrik <Henriksaega@voco.ee>
2025-09-19 09:58:15 +0300

    Exercise12.1 - Storehouse

---
+++ src/week8/Storehouse.java
@@ -0,0 +1,28 @@
+package week8;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Storehouse {
+    private Map<String, Integer> storehouse;
+    private Map<String, Integer> product;
+    private Map<String, Integer> price;
+    private Map<String, Integer> stock;
+
+    public Storehouse() {
+        storehouse = new HashMap<String, Integer>();
+        product = new HashMap<String, Integer>();
+        price= new HashMap<String, Integer>();
+        stock = new HashMap<String, Integer>();
+    }
+    public void addProduct(String product, int price, int stock){
+         storehouse.put(product, price);
+         this.price.put(product, price);
+        this.stock.put(product, stock);
+    }
+    public int price(String product){
+        if(storehouse.containsKey(product)){
+           return price.getOrDefault(product, -99);
+        }
+        return -99;
+    }
+}
