0cf081ff67e9acaddbb1f8a73b627bbc9179906a
Henrik <Henriksaega@voco.ee>
2025-09-25 14:01:15 +0300

    Exercise7.5 - Adding and Translarting Words

--- src/week7/TextUserInterface.java
+++ src/week7/TextUserInterface.java
@@ -13,6 +13,10 @@ public class TextUserInterface {
 
     public void start() {
         do{
+            System.out.println("\nStatements:\n" +
+                    "  add - adds a word pair to the dictionary\n" +
+                    "  translate - asks a word and prints its translation\n" +
+                    "  quit - quits the text user interface");
             System.out.print("Statement: ");
             String input = scanner.nextLine();
 
@@ -22,6 +26,34 @@ public class TextUserInterface {
             } else {
                 System.out.println("Unknown statement");
             }
+
+            if(input.equalsIgnoreCase("add")){
+                String sõna;
+                String vaste;
+                System.out.print("In Estonia: ");
+                sõna = scanner.nextLine();
+                System.out.print("Translation: ");
+                vaste = scanner.nextLine();
+                this.add(sõna, vaste);
+            }
+            else if (input.equalsIgnoreCase("translate")){
+                String sõna;
+                System.out.print("Give a word: ");
+                sõna = scanner.nextLine();
+                if(this.dictionary.containsKey(sõna)){
+                    this.translate(sõna);
+                } else{
+                    System.out.println("Sorry no such word!");
+                }
+            }
         } while(true);
+    }
+
+    public void add(String sõna, String vaste){
+         this.dictionary.put(sõna,vaste);
+         System.out.println("Word added successfully!");
+    }
+    public void translate(String sõna){
+        System.out.print("Translation: " + this.dictionary.get(sõna) + "\n");
     }
 }
